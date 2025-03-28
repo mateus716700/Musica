4cdd4ae66775116373af7d7deee175cb313d00f9
Mateus Fernandes <mateusfernandes131004@gmail.com>
2025-03-28 20:25:16 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("miragem");
+Console.WriteLine();
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"Eu não acredito, só pode ser uma miragem
+Eu não acredito no que os meus olhos estão vendo
+Eu não acredito, só pode ser uma miragem
+Eu não acredito no que os meus olhos estão vendo
+Eu não acredito, só pode ser uma miragem");
+Console.ResetColor();
